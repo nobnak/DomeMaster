b0fd59fd5fe130925f8111fa58898a626d6fb9e9
Nakata Nobuyuki <nobu.nakata@gmail.com>
2016-04-16 02:48:50 +0900

    + Face mask

--- Assets/Packages/DomeMaster/DomeMaster.cs
+++ Assets/Packages/DomeMaster/DomeMaster.cs
@@ -5,18 +5,31 @@ namespace DomeMasterSystem {
 	[ExecuteInEditMode]
 	[RequireComponent(typeof(Camera))]
 	public class DomeMaster : MonoBehaviour {
+		[System.Flags]
+		public enum FaceMaskFlags { 
+			Right = 1 << 0, 
+			Left = 1 << 1, 
+			Top = 1 << 2, 
+			Bottom = 1 << 3, 
+			Front = 1 << 4,
+			Back = 1 << 5 }
+
 		public const string PROP_DOME_MASTER_CUBE = "_DomeMasterCube";
 		public const string PROP_DIRECTION = "_Dir";
 		public enum DirEnum { Front = 1, Back = -1 }
 		public TextureEvent OnUpdateCubemap;
 		public bool generateMips = false;
 		public int lod = 10;
+		[InspectorFlags]
+		public FaceMaskFlags faceMask;
 
 		Camera _attachedCam;
 		RenderTexture _cube;
 
 		void OnEnable() {
 			_attachedCam = GetComponent<Camera> ();
+
+			Debug.LogFormat ("CubemapFace {0}={1}", CubemapFace.PositiveX, (int)CubemapFace.PositiveX);
 		}
 		void Update() {
 			var res = (1 << Mathf.Clamp(lod, 1, 13));
@@ -31,7 +44,7 @@ namespace DomeMasterSystem {
 				_cube.Create ();
 			}
 			_attachedCam.enabled = false;
-			_attachedCam.RenderToCubemap (_cube);
+			_attachedCam.RenderToCubemap (_cube, (int)faceMask);
 			Shader.SetGlobalTexture(PROP_DOME_MASTER_CUBE, _cube);
 
 			OnUpdateCubemap.Invoke (_cube);
---
+++ Assets/Packages/DomeMaster/InspectorFlags.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace DomeMasterSystem {
+	[System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Enum)]
+	public sealed class InspectorFlagsAttribute : PropertyAttribute {}
+
+	#if UNITY_EDITOR
+	[CustomPropertyDrawer(typeof(InspectorFlagsAttribute))]
+	public sealed class InspectorFlagsAttributeDrawer : PropertyDrawer {
+		public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+			property.intValue = EditorGUI.MaskField (position, label, property.intValue, property.enumNames);
+		}
+	}
+	#endif
+}
